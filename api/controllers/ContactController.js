75407a7e2b2f65ac8f6cff1bbe6fe7b56bf10cd5
mahongquan <mahongquan@sina.com>
2016-07-06 15:08:16 +0800

    find.js

--- api/controllers/ContactController.js
+++ api/controllers/ContactController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var actionUtil = require('./actionUtil'), _ = require('lodash');
 module.exports = {
   login: function (req, res) {
     return res.send("Hi there!");
@@ -14,6 +14,44 @@ module.exports = {
   },
   signup: function (req, res) {
     return res.redirect("http://www.baidu.com");
+  },
+  find:function(req,res){
+  	console.log("find===================");
+  	// Look up the model
+  var Model = actionUtil.parseModel(req);
+
+
+  // If an `id` param was specified, use the findOne blueprint action
+  // to grab the particular instance with its primary key === the value
+  // of the `id` param.   (mainly here for compatibility for 0.9, where
+  // there was no separate `findOne` action)
+  if ( actionUtil.parsePk(req) ) {
+    return require('./findOne')(req,res);
   }
+
+  // Lookup for records that match the specified criteria
+  var query = Model.find()
+  .where( actionUtil.parseCriteria(req) )
+  .limit( actionUtil.parseLimit(req) )
+  .skip( actionUtil.parseSkip(req) )
+  .sort( actionUtil.parseSort(req) );
+  query = actionUtil.populateRequest(query, req);
+  query.exec(function found(err, matchingRecords) {
+    if (err) return res.serverError(err);
+
+    // Only `.watch()` for new instances of the model if
+    // `autoWatch` is enabled.
+    if (req._sails.hooks.pubsub && req.isSocket) {
+      Model.subscribe(req, matchingRecords);
+      if (req.options.autoWatch) { Model.watch(req); }
+      // Also subscribe to instances of all associated models
+      _.each(matchingRecords, function (record) {
+        actionUtil.subscribeDeep(req, record);
+      });
+    }
+
+    res.ok(matchingRecords);
+  });
+  },
 };
 
