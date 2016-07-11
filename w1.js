34d3da711b81a051eb6e20dbcfabc670b9653ac1
mahongquan <mahongquan@sina.com>
2016-07-11 19:17:06 +0800

    waterline

--- w1.js
+++ w1.js
@@ -1,16 +1,51 @@
 var Waterline = require('waterline');
 var sailsDiskAdapter = require('sails-disk');
 var waterline = new Waterline();
-var Contact=require("./api/models/Contact.js");
-// var Contact=require("./api/models/Contact.js");
-// var Contact=require("./api/models/Contact.js");
-console.log(contact);
-var contactCollection = Waterline.Collection.extend({
+var Pack = Waterline.Collection.extend({
+    identity: 'pack',
+    name:'pack',
+    connection: 'localDiskDb',
+    attributes: {
+    name :{type:'string'},// models.CharField(max_length=30,verbose_name="用户单位")#用户单位
+  }
+});
+var Usepack = Waterline.Collection.extend({
+    identity: 'usepack',
+    name:'usepack',
+    connection: 'localDiskDb',
+  attributes: {
+    name :{type:'string'},// models.CharField(max_length=30,verbose_name="用户单位")#用户单位
+    contact: {
+      model: 'contact'
+    },
+    pack: {
+      model: 'pack'
+    }
+  }
+});
+var Contact = Waterline.Collection.extend({
     identity: 'contact',
     connection: 'localDiskDb',
-    attributes: Contact.attributes
+   attributes: {
+     yonghu :{type:'string'},// models.CharField(max_length=30,verbose_name="用户单位")#用户单位
+     addr:{type:'string'},// = models.CharField(max_length=30,verbose_name="客户地址",null=True,blank=True)#用户单位
+     channels:{type:'string'},// = models.CharField(max_length=30,verbose_name="通道配置",null=True,blank=True)#用户单位
+     yiqixinghao:{type:'string'},//=models.CharField(max_length=30,verbose_name="仪器型号",choices=ACHOICE)#仪器型号
+     yiqibh:{type:'string'},//=models.CharField(max_length=30,verbose_name="仪器编号")#仪器编号
+     baoxiang:{type:'string'},// =  models.CharField(max_length=30,verbose_name="包箱")#包箱
+     shenhe:{type:'string'},// =  models.CharField(max_length=30,verbose_name="审核")#审核
+     yujifahuo_date:{type:'string'},// = models.DateTimeField(verbose_name="预计发货时间")#预计发货时间
+     hetongbh:{type:'string'},//=models.CharField(max_length=30,verbose_name="合同编号")#合同编号
+     tiaoshi_date:{type:'string'},
+     usepacks: {
+      collection: 'usepack',
+      via: 'contact'
+    }
+  }
 });
-waterline.loadCollection(contactCollection);
+waterline.loadCollection(Pack);
+waterline.loadCollection(Usepack);
+waterline.loadCollection(Contact);
 var config = {
     adapters: {
         'sails-disk': sailsDiskAdapter
@@ -23,34 +58,26 @@ var config = {
         }
     }
 };
+var showcontact=function(contact){
+	console.log(contact.id);
+};
 waterline.initialize(config, function (err, ontology) {
     if (err) {
         return console.error(err);
     }
-
     // Tease out fully initialised models.
-    // var User = ontology.collections.user;
-    // var Pet = ontology.collections.pet;
+    var Contact = ontology.collections.contact;
+    //console.log(Contact);
+    Contact.find()
+	.populate('usepacks')
+	.exec(function(err, contacts) {
+		  if(err)   {
 
-    // User.create({ // First we create a user.
-    //         firstName: 'Neil',
-    //         lastName: 'Armstrong'
-    //     }).then(function (user) { // Then we create the pet
-    //         return Pet.create({
-    //             breed: 'beagle',
-    //             type: 'dog',
-    //             name: 'Astro',
-    //             owner: user.id
-    //         });
-
-    //     }).then(function (pet) { // Then we grab all users and their pets
-    //         return User.find().populate('pets');
-
-    //     }).then(function(users){ // Results of the previous then clause are passed to the next
-    //     	console.log('users');
-    //         console.dir(users);
-
-    //     }).catch(function(err){ // If any errors occur execution jumps to the catch block.
-    //         console.error(err);
-    //     });
+		  }
+		  else{
+			  for(var i in contacts){
+			  	showcontact(contacts[i]);
+			  }
+		}
+	});
 });
